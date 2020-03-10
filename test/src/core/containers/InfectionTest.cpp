feda328f85aaa2acb744011480a7418bdd585416
Max Murphy <murphy2122@berkeley.edu>
2020-03-10 12:48:36 -0700

    update infection test to make sure multiple loci works

--- test/src/core/containers/InfectionTest.cpp
+++ test/src/core/containers/InfectionTest.cpp
@@ -12,22 +12,40 @@ TEST(InfectionTest, HandlesChangedLatentAlleles) {
     using GeneticsImpl = AllelesBitSet<16>;
 
     Locus as1("AS1", 6);
+    Locus as2("AS2", 8);
     bool allelesChanged = false;
 
-    std::vector<LocusGeneticsAssignment<GeneticsImpl, Locus>> dlas{{&as1, GeneticsImpl("011010")}};
-    std::vector<LocusGeneticsAssignment<GeneticsImpl, Locus>> plas{{&as1, GeneticsImpl("011010")}};
+    std::vector<LocusGeneticsAssignment<GeneticsImpl, Locus>> dlas{{&as1, GeneticsImpl("011010")},
+                                                                   {&as2, GeneticsImpl("00000011")}};
+
+    std::vector<LocusGeneticsAssignment<GeneticsImpl, Locus>> plas{{&as1, GeneticsImpl("011010")},
+                                                                   {&as2, GeneticsImpl("00000011")}};
+
     Infection<GeneticsImpl> inf1(dlas, plas);
+    inf1.add_post_change_listener([&]() {allelesChanged = true; });
 
-    inf1.add_post_change_listener([&]() { allelesChanged = true; });
-
-    auto tmp = inf1.latentGenotype(&as1).value();
-    tmp.flip(0);
-    ASSERT_FALSE(allelesChanged);
+    allelesChanged = false;
+    auto tmp1 = inf1.latentGenotype(&as1).value();
+    tmp1.flip(0);
+    EXPECT_FALSE(allelesChanged);
 
     inf1.latentGenotype(&as1).saveState();
-    inf1.latentGenotype(&as1).setValue(tmp);
+    inf1.latentGenotype(&as1).setValue(tmp1);
+    EXPECT_TRUE(allelesChanged);
+    EXPECT_EQ(inf1.latentGenotype(&as1).value().allelesStr(), "111010");
+    EXPECT_EQ(inf1.observedGenotype(&as1).value().allelesStr(), "011010");
+    inf1.latentGenotype(&as1).acceptState();
 
-    ASSERT_TRUE(allelesChanged);
-    ASSERT_EQ(inf1.latentGenotype(&as1).value().allelesStr(), "011011");
-    ASSERT_EQ(inf1.observedGenotype(&as1).value().allelesStr(), "011010");
+    allelesChanged = false;
+    auto tmp2 = inf1.latentGenotype(&as2).value();
+    tmp2.flip(0);
+    EXPECT_FALSE(allelesChanged);
+
+    inf1.latentGenotype(&as2).saveState();
+    inf1.latentGenotype(&as2).setValue(tmp2);
+
+    EXPECT_TRUE(allelesChanged);
+    EXPECT_EQ(inf1.latentGenotype(&as2).value().allelesStr(), "10000011");
+    EXPECT_EQ(inf1.observedGenotype(&as2).value().allelesStr(), "00000011");
+    inf1.latentGenotype(&as2).acceptState();
 }
